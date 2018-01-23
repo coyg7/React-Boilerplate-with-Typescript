28e288438b989c9a9d659c009b57b3f4ccfcd98a
coyg7 <ssachit3761@gmail.com>
2018-01-23 12:17:54 +0530

    Convert index.js to tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" onClick={e => {console.log(e);}}>
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
